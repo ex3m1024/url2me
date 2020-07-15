c84ee0c206cae9136907b758824d27f798dcc96c
kit <nikita.rt@neueda.com>
2020-07-15 13:12:58 +0300

    init project

---
+++ src/test/kotlin/rt/kit/url2me/Url2meApplicationTests.kt
@@ -0,0 +1,13 @@
+package rt.kit.url2me
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Url2meApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
