4ad1a3088ccae867beeafddbf9fed214a4a422a9
kit <nikita.rt@neueda.com>
2020-07-22 20:24:34 +0300

    Specification: using shorter response messages; basic Exception handling

--- src/main/kotlin/rt/kit/url2me/controller/MainController.kt
+++ src/main/kotlin/rt/kit/url2me/controller/MainController.kt
@@ -1,6 +1,9 @@
 package rt.kit.url2me.controller
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import rt.kit.url2me.dto.UrlInput
 import rt.kit.url2me.dto.UrlOutput
@@ -12,11 +15,9 @@ import javax.validation.Valid
 class MainController(val service: ShortenerService, @Value("\${urldomain}") val domain: String) {
 
     @PostMapping("generate")
-    fun generate(@Valid @RequestBody body: UrlInput) = UrlOutput("$domain/${service.shorten(body.url)}")
-
-    // TODO: Exception classes, ExceptionHandler
+    fun generate(@Valid @RequestBody body: UrlInput) = ResponseEntity(UrlOutput("$domain/${service.shorten(body.url)}"), HttpStatus.OK)
 
     @GetMapping("{code}")
-    fun resolve(@PathVariable(value = "code") code: String) = "TODO: Redirecting to ${service.resolve(code)}..."
-
+    fun resolve(@PathVariable(value = "code") code: String) =
+            ResponseEntity<Any>(HttpHeaders().also { it.add("Location", service.resolve(code)) }, HttpStatus.MOVED_PERMANENTLY)
 }
