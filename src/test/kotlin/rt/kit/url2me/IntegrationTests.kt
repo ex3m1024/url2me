90ee62e59bb485eb124d0aea4155676b2a72b7e6
kit <nikita.rt@neueda.com>
2020-07-24 17:59:43 +0300

    Basic integration tests for generating and resolving URLs

--- src/test/kotlin/rt/kit/url2me/IntegrationTests.kt
+++ src/test/kotlin/rt/kit/url2me/IntegrationTests.kt
@@ -4,20 +4,50 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.core.publisher.Mono
 import rt.kit.url2me.controller.MainController
+import rt.kit.url2me.dto.UrlInput
+
+private const val URI_GENERATE = "/generate"
+private const val URI_RESOLVE_VALID = "/abcd"
+private const val VALID_URL = "https://google.com"
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTests(@Autowired val controller: MainController) {
 
     @Test
-    fun test() {
+    fun `Assert shortened URL is generated correctly`() {
+        WebTestClient
+            .bindToController(controller)
+            .build()
+            .post()
+            .uri(URI_GENERATE)
+            .body(Mono.just(UrlInput(VALID_URL)), UrlInput::class.java)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("url").isNotEmpty
+    }
+
+    @Test
+    fun `Assert shortened URL is not generated if URL is not supplied`() {
+        WebTestClient
+            .bindToController(controller)
+            .build()
+            .post()
+            .uri(URI_GENERATE)
+            .exchange()
+            .expectStatus().isBadRequest
+    }
+
+    @Test
+    fun `Assert shortened URL is resolved correctly`() {
         WebTestClient
             .bindToController(controller)
             .build()
             .get()
-            .uri("/generate")
+            .uri(URI_RESOLVE_VALID)
             .exchange()
             .expectStatus().isOk
-        // TODO: cover /generate and /{code} with simple integration tests
     }
 }
