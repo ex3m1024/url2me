ddff5ef6c79f48214d68118cd918fd39be77599a
kit <nikita.rt@neueda.com>
2020-07-29 18:09:09 +0300

    resolve() function refactoring

--- src/main/kotlin/rt/kit/url2me/controller/MainController.kt
+++ src/main/kotlin/rt/kit/url2me/controller/MainController.kt
@@ -1,7 +1,6 @@
 package rt.kit.url2me.controller
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -18,6 +17,11 @@ class MainController(val service: ShortenerService, @Value("\${urldomain}") val 
     fun generate(@Valid @RequestBody body: UrlInput) = ResponseEntity(UrlOutput("$domain/${service.shorten(body.url)}"), HttpStatus.OK)
 
     @GetMapping("{code}")
-    fun resolve(@PathVariable(value = "code") code: String) =
-            ResponseEntity<Any>(HttpHeaders().also { it.add("Location", service.resolve(code)) }, HttpStatus.MOVED_PERMANENTLY)
+    fun resolve(@PathVariable(value = "code") code: String): ResponseEntity<Any> =
+        service.resolve(code).let { url ->
+            ResponseEntity
+                .status(HttpStatus.MOVED_PERMANENTLY)
+                .header("Location", url)
+                .build()
+        }
 }
