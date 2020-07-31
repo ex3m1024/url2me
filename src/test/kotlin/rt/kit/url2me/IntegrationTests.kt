a1fce24be97d8947c00c5126118fac32c9532520
kit <nikita.rt@neueda.com>
2020-07-31 18:54:14 +0300

    Fix test

--- src/test/kotlin/rt/kit/url2me/IntegrationTests.kt
+++ src/test/kotlin/rt/kit/url2me/IntegrationTests.kt
@@ -3,6 +3,7 @@ package rt.kit.url2me
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 import reactor.core.publisher.Mono
 import rt.kit.url2me.controller.MainController
@@ -48,6 +49,6 @@ class IntegrationTests(@Autowired val controller: MainController) {
             .get()
             .uri(URI_RESOLVE_VALID)
             .exchange()
-            .expectStatus().isOk
+            .expectStatus().is3xxRedirection
     }
 }
